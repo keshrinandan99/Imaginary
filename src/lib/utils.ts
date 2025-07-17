6c99e31f62b6feb375ecd96c67bd0dd9727dc5d7
nandan <sameernandan44@gmail.com>
2025-07-17 17:02:23 +0530

    added clerk

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,5 @@
-/* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
 import { twMerge } from "tailwind-merge";
-
-import { aspectRatioOptions } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
