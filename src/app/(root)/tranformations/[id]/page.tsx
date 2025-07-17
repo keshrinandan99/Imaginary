6c8c5c876b652a1b36663bd875f22f5a3ba164d1
nandan <sameernandan44@gmail.com>
2025-07-17 15:33:09 +0530

    structure ready

---
+++ src/app/(root)/tranformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TransformationPage from '../page'
+
+function  TransformationPageID () {
+  return (
+    <div></div>
+  )
+}
+
+export default TransformationPageID
